96c38a7323e6c43ce3e0448fca33ab067de2a61e
Kevin Vu <kevintvu123@gmail.com>
2023-04-24 08:58:36 -0400

    component setup and styling

---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,11 @@
+const MessageDisplay = () => {
+    return (
+      <div className="message-display">
+        <p id="icon">X</p>
+        <p>user</p>
+        <p>message</p>
+      </div>
+    );
+  }
+  
+  export default MessageDisplay
---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,13 @@
+import MessageDisplay from "./MessageDisplay";
+
+const MessagesDisplay = () => {
+    return (
+      <div className="messages-display">
+        <MessageDisplay />
+        <MessageDisplay />
+        <MessageDisplay />
+      </div>
+    );
+  }
+  
+  export default MessagesDisplay
